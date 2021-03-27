86fbe33a8f398d1dc0c83c9db9934d3086f06b2c
Aaradhya-tech <70269882+Aaradhya-tech@users.noreply.github.com>
2021-03-27 14:41:53 +0530

    Add files via upload

---
+++ Coin.js
@@ -0,0 +1,23 @@
+class Coin{
+    constructor(a,b,r){
+    var options = {
+        restitution :1.0
+     }
+   this.bodies= Bodies.circle(a,b,r);
+   this.width = a;
+   this.height = b;
+   this.rad = r;
+   this.image = loadImage("coin.png")
+    World.add(myworld, this.bodies);
+    }
+
+
+    display(){
+            var pos = this.bodies.position;
+            push();
+            translate(pos.x, pos.y);
+            imageMode(CENTER) 
+            image(this.image,0,0,this.rad,this.rad);
+            pop();
+    }
+}
